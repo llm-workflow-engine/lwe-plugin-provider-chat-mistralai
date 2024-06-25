d3f78462d64ba1b22a6412f0d16dd9cf077266b3
Chad Phillips <chad@apartmentlines.com>
2024-06-25 11:24:53 -0400

    refactor for data caching support

--- lwe_plugin_provider_chat_mistralai/plugin.py
+++ lwe_plugin_provider_chat_mistralai/plugin.py
@@ -27,43 +27,46 @@ class ProviderChatMistralai(Provider):
         return {
             "chat": True,
             'validate_models': True,
-            'models': {
-                'open-mistral-7b': {
-                    'max_tokens': 32768,
-                },
-                'open-mixtral-8x7b': {
-                    'max_tokens': 32768,
-                },
-                'open-mixtral-8x22b': {
-                    'max_tokens': 65536,
-                },
-                'mistral-tiny': {
-                    'max_tokens': 32768,
-                },
-                'mistral-small': {
-                    'max_tokens': 32768,
-                },
-                'mistral-small-latest': {
-                    'max_tokens': 32768,
-                },
-                'mistral-medium': {
-                    'max_tokens': 32768,
-                },
-                'mistral-medium-latest': {
-                    'max_tokens': 32768,
-                },
-                'mistral-large-latest': {
-                    'max_tokens': 32768,
-                },
-                'codestral-latest': {
-                    'max_tokens': 32768,
-                },
-            },
         }
 
     @property
     def default_model(self):
         return MISTRAL_AI_DEFAULT_MODEL
+
+    @property
+    def static_models(self):
+        return {
+            'open-mistral-7b': {
+                'max_tokens': 32768,
+            },
+            'open-mixtral-8x7b': {
+                'max_tokens': 32768,
+            },
+            'open-mixtral-8x22b': {
+                'max_tokens': 65536,
+            },
+            'mistral-tiny': {
+                'max_tokens': 32768,
+            },
+            'mistral-small': {
+                'max_tokens': 32768,
+            },
+            'mistral-small-latest': {
+                'max_tokens': 32768,
+            },
+            'mistral-medium': {
+                'max_tokens': 32768,
+            },
+            'mistral-medium-latest': {
+                'max_tokens': 32768,
+            },
+            'mistral-large-latest': {
+                'max_tokens': 32768,
+            },
+            'codestral-latest': {
+                'max_tokens': 32768,
+            },
+        }
 
     def prepare_messages_method(self):
         return self.prepare_messages_for_llm_chat
